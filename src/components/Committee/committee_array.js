bb453bcdc78cd8a23520ac9fd172db35b48155fd
Ethan <ethanchen2009@hotmail.com>
2022-10-31 15:16:20 +0800

    Added Li Yang

--- src/components/Committee/committee_array.js
+++ src/components/Committee/committee_array.js
@@ -4,7 +4,8 @@ import president from "../../assets/committee_pics/Presidential/Nathan.png";
 import vice_president1 from "../../assets/committee_pics/Presidential/Zi Yi.png";
 import vice_president2 from "../../assets/committee_pics/Presidential/Hao Yi.png";
 import admin from "../../assets/committee_pics/Admin/Michelle.png";
-import event from "../../assets/committee_pics/Events/Deren.png";
+import event1 from "../../assets/committee_pics/Events/Deren.png";
+import event2 from "../../assets/committee_pics/Events/Li Yang.png";
 import outreach1 from "../../assets/committee_pics/Outreach/Yee Thern.png";
 import outreach2 from "../../assets/committee_pics/Outreach/Tze Wei.png";
 import outreach3 from "../../assets/committee_pics/Outreach/Hao Jun.png";
@@ -29,7 +30,11 @@ const committeeArray = [
   },
   {
     position: "Event Head",
-    personArray: [{ name: "Cheng Deren", pic: event }],
+    personArray: [{ name: "Cheng Deren", pic: event1 }],
+  },
+  {
+    position: "Event Vice Head",
+    personArray: [{ name: "Lau Li Yang", pic: event2 }],
   },
   {
     position: "Public Relation and Outreach Head",
